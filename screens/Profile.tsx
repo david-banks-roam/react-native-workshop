04fb701d575a7c16e9398f9bb09ed1a26e07fc73
Laurent Lubino <laurent.lubino@weareroam.com>
2021-08-22 11:17:10 +1200

    remove code for starter

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,32 +1,20 @@
-import * as React from 'react';
-import { StyleSheet, Image } from 'react-native';
-import { Text, View } from '../components/Themed';
-import { useProfile } from '../contexts/ProfileContext';
+import * as React from "react";
+import { StyleSheet, Image } from "react-native";
+import { Text, View } from "../components/Themed";
+import { useProfile } from "../contexts/ProfileContext";
 
-export default function TabTwoScreen() {
-    const { profile } = useProfile();
-
-    return (
-        <View style={styles.container}>
-            <Text>{profile.name}</Text>
-            {!!profile.picture && <Image source={profile.picture} />}
-        </View>
-    );
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Profile</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
